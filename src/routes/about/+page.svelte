09cc296f9037dd2e6182c0725dd0eae8b9f83038
Dan Schultz <slifty@gmail.com>
2024-03-20 23:05:31 -0400

    Add initial content and framework
    This commit is very large, that's because it:
    
    1. Sets up TypeScript
    2. Sets up Svelte
    3. Sets up GitHub workflows
    
    Commits can get a bit more granular from here.

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,357 @@
+<div id="about">
+	<p>
+		The Cheltenham Democratic Committee is a political organization based in Cheltenham Township (<a
+			href="https://slifty.github.io/cheltenham-ward-map/">Ward Map</a
+		>). We are dedicated to promoting and supporting the Democratic Party's values and candidates in
+		our community.
+	</p>
+</div>
+
+<h2>Governing Documents</h2>
+<ul>
+	<li>
+		<a href="https://drive.google.com/file/d/1ZJEcmW1WzSVdmhr1hD00SqF4AM5AyUPY/view?usp=sharing"
+			>Bylaws</a
+		>
+	</li>
+	<li>
+		<a href="https://drive.google.com/file/d/1lLn6gO21Mj4C40gsv7qEL3Y1h54oUG0Z/view?usp=sharing"
+			>Conflict of Interest Policy</a
+		>
+	</li>
+</ul>
+
+<h2>Officers</h2>
+<div id="officers">
+	<ul>
+		<li id="chairs">
+			<h3>Co-Chairs</h3>
+			<div class="officer">Julie Haywood</div>
+			<div class="officer">Daniel Schultz</div>
+		</li>
+		<li>
+			<h3>1st Vice Chair</h3>
+			<div class="officer">Leah Mulhearn</div>
+		</li>
+		<li>
+			<h3>2nd Vice Chair</h3>
+			<div class="officer">Joyce Drayton</div>
+		</li>
+		<li>
+			<h3>Corresponding Secretary</h3>
+			<div class="officer">Chloe Woodrow</div>
+		</li>
+		<li>
+			<h3>Recording Secretary</h3>
+			<div class="officer">Diana Medley</div>
+		</li>
+		<li>
+			<h3>Treasurer</h3>
+			<div class="officer">Ted Cerebi</div>
+		</li>
+		<li>
+			<h3>Parliamentarian</h3>
+			<div class="officer">William Pillsbury</div>
+		</li>
+		<li>
+			<h3>Representative At Large</h3>
+			<div class="officer">Dwight Pedro Lewis</div>
+		</li>
+		<li />
+	</ul>
+</div>
+
+<h2>Committee People</h2>
+<div id="committee-people">
+	<ul id="wards">
+		<li>
+			<h3>Ward 1</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 1-1</h4>
+					<div class="committee-person">Terri Davis</div>
+					<div class="committee-person ward-leader">Martin Schmid (ward leader)</div>
+				</li>
+				<li>
+					<h4>Precinct 1-2</h4>
+					<div class="committee-person">Deborah McCarter</div>
+					<div class="committee-person">Jeffrey Williams</div>
+				</li>
+				<li>
+					<h4>Precinct 1-3</h4>
+					<div class="committee-person">Meika P. Jones</div>
+					<div class="committee-person">Vanessa McKenzie</div>
+				</li>
+				<li>
+					<h4>Precinct 1-4</h4>
+					<div class="committee-person" />
+					<div class="committee-person" />
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ward 2</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 2-1</h4>
+					<div class="committee-person ward-leader">Wendy Blutstein (ward leader)</div>
+					<div class="committee-person">Dwight Pedro Lewis</div>
+				</li>
+				<li>
+					<h4>Precinct 2-2</h4>
+					<div class="committee-person">Bruce Wagner</div>
+					<div class="committee-person">Lisa Fuller</div>
+				</li>
+				<li>
+					<h4>Precinct 2-3</h4>
+					<div class="committee-person">Daphne Oliver</div>
+					<div class="committee-person">Craig C. Browne</div>
+				</li>
+				<li>
+					<h4>Precinct 2-4</h4>
+					<div class="committee-person">Regina Gordon</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ward 3</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 3-1</h4>
+					<div class="committee-person">Ted Cerebi</div>
+					<div class="committee-person">Allison Martin Nguyen</div>
+				</li>
+				<li>
+					<h4>Precinct 3-2</h4>
+					<div class="committee-person ward-leader">Joe Kappler (ward leader)</div>
+					<div class="committee-person">Ken Mirsky</div>
+				</li>
+				<li>
+					<h4>Precinct 3-3</h4>
+					<div class="committee-person">Brad Pransky</div>
+					<div class="committee-person">Leah Mulhearn</div>
+				</li>
+				<li>
+					<h4>Precinct 3-4</h4>
+					<div class="committee-person">Geraldine (Gerry) Brown</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ward 4</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 4-1</h4>
+					<div class="committee-person">Sandra L. Jenkins</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+				<li>
+					<h4>Precinct 4-2</h4>
+					<div class="committee-person">Sandra Edwards</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+				<li>
+					<h4>Precinct 4-3</h4>
+					<div class="committee-person vacant">(Vacant)</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+				<li>
+					<h4>Precinct 4-4</h4>
+					<div class="committee-person ward-leader">Steve Burns (ward leader)</div>
+					<div class="committee-person">Andrew D. Montroy</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ward 5</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 5-1</h4>
+					<div class="committee-person">Fred Milbert</div>
+					<div class="committee-person">William Pillsbury</div>
+				</li>
+				<li>
+					<h4>Precinct 5-2</h4>
+					<div class="committee-person">Carla Davis Cunningham</div>
+					<div class="committee-person">Caryl T. Levin</div>
+				</li>
+				<li>
+					<h4>Precinct 5-3</h4>
+					<div class="committee-person">Matthew McAlpin</div>
+					<div class="committee-person">Linda Feldman</div>
+				</li>
+				<li>
+					<h4>Precinct 5-4</h4>
+					<div class="committee-person ward-leader">Joyce Drayton (ward leader)</div>
+					<div class="committee-person">Daniel Norris</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ward 6</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 6-1</h4>
+					<div class="committee-person ward-leader">Eileen Rall (ward leader)</div>
+					<div class="committee-person">Gretchen Wisehart</div>
+				</li>
+				<li>
+					<h4>Precinct 6-2</h4>
+					<div class="committee-person vacant">(Vacant)</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+				<li>
+					<h4>Precinct 6-3</h4>
+					<div class="committee-person">Nancy Smoler</div>
+					<div class="committee-person">Mitch Zygmund-Felt</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ward 7</h3>
+			<ul class="precincts">
+				<li>
+					<h4>Precinct 7-1</h4>
+					<div class="committee-person ward-leader">Linda P. Musumeci (ward leader)</div>
+					<div class="committee-person vacant">(Vacant)</div>
+				</li>
+				<li>
+					<h4>Precinct 7-2</h4>
+					<div class="committee-person">Marc Lieberson</div>
+					<div class="committee-person">Donna Johnson Payne</div>
+				</li>
+				<li>
+					<h4>Precinct 7-3</h4>
+					<div class="committee-person">Mary Russell</div>
+					<div class="committee-person">Irv Brockington</div>
+				</li>
+			</ul>
+		</li>
+		<li />
+	</ul>
+</div>
+
+<h2>Elected Officials</h2>
+<ul>
+	<li><a href="https://www.cheltenham.org/domain/180">School Board</a></li>
+	<li>
+		<a href="https://www.cheltenhamtownship.org/pview.aspx?id=2426&catid=26#gsc.tab=0"
+			>Township Commissioners</a
+		>
+	</li>
+	<li>
+		<a href="https://www.cheltenhamtownship.org/pview.aspx?id=2423#gsc.tab=0"
+			>Finance Officer Mark Lieberson</a
+		>
+	</li>
+	<li>
+		<a
+			href="https://www.legis.state.pa.us/cfdocs/legis/home/member_information/house_bio.cfm?id=1914"
+			>PA State Representative Napoleon Nelson</a
+		>
+	</li>
+	<li>
+		<a
+			href="https://www.legis.state.pa.us/cfdocs/legis/home/member_information/senate_bio.cfm?id=1689"
+			>PA State Senator Art Haywood</a
+		>
+	</li>
+	<li><a href="https://dean.house.gov/biography">Congresswoman Madeleine Deane</a></li>
+	<li><a href="https://www.casey.senate.gov/">U.S. Senator Bob Casey</a></li>
+	<li><a href="https://www.fetterman.senate.gov/">U.S. Senator John Fetterman</a></li>
+	<li><a href="https://www.governor.pa.gov/">PA Governor Josh Shapiro</a></li>
+</ul>
+
+<h2>Government</h2>
+<ul>
+	<li><a href="https://www.cheltenhamtownship.org">Cheltenham Township</a></li>
+	<li><a href="https://www.cheltenham.org">Cheltenham School District</a></li>
+	<li><a href="https://www.montgomerycountypa.gov/">Montgomery County</a></li>
+	<li><a href="https://www.pa.gov/">Pennsylvania</a></li>
+	<li><a href="https://www.usa.gov">United States</a></li>
+</ul>
+
+<h2>Democratic Party</h2>
+<ul>
+	<li><a href="https://mcdems.org/">Montgomery County Committee</a></li>
+	<li><a href="https://www.padems.com/">Pennsylvania Committee</a></li>
+	<li><a href="https://democrats.org/">National Committee</a></li>
+</ul>
+
+<style>
+	#officers ul {
+		list-style: none;
+		display: flex;
+		flex-wrap: wrap;
+		border: 4px solid #eee;
+		padding: 5px;
+		margin: 0px;
+	}
+	#officers li {
+		flex: 1 0 calc(50% - 20px);
+		display: block;
+		width: 50%;
+		min-width: 300px;
+		padding: 5px;
+		margin: 0px;
+		background-color: #ffffff;
+	}
+	#officers li#chairs {
+		flex: 1 0 calc(100% - 20px);
+		width: 100%;
+	}
+	#officers .officer {
+		text-align: center;
+	}
+
+	#wards {
+		display: flex;
+		flex-wrap: wrap;
+		list-style: none;
+		padding: 0px;
+		margin: 0px;
+		border: 4px solid #eee;
+	}
+	#wards > li {
+		flex: 1 0 calc(50% - 10px);
+		display: block;
+		width: 50%;
+		padding: 5px;
+		min-width: 300px;
+		margin: 0px;
+	}
+
+	ul.precincts {
+		list-style: none;
+		padding: 0px;
+	}
+	ul.precincts > li {
+		margin: 0px;
+		margin-bottom: 20px;
+	}
+	.committee-person {
+		text-align: center;
+	}
+
+	h2 {
+		margin: 5px 0px;
+		margin-top: 20px;
+		padding: 0px;
+	}
+
+	h3 {
+		text-align: center;
+		background-color: #f9d95c;
+		padding: 3px;
+		margin: 5px 0px;
+	}
+
+	h4 {
+		margin: 5px 0px;
+		text-align: center;
+		background-color: #eee;
+		padding: 2px;
+	}
+</style>
---
+++ src/routes/vote/+page.svelte
@@ -0,0 +1,237 @@
+<h1>Vote</h1>
+<h2>Voter Resources</h2>
+<ul>
+	<li><a href="https://mcdems.org/votingcenter/">Montgomery County Democrats voting center</a>.</li>
+	<li>
+		<a href="https://www.vote.pa.gov/Pages/default.aspx">PA voting and election information</a>.
+	</li>
+	<li>
+		<a href="https://www.pavoterservices.pa.gov/Pages/VoterRegistrationApplication.aspx"
+			>Register to vote</a
+		>.
+	</li>
+	<li>
+		<a href="https://www.vote.pa.gov/Voting-in-PA/Pages/Mail-and-Absentee-Ballot.aspx"
+			>Apply for a mail-in or absentee ballot.</a
+		>
+	</li>
+</ul>
+
+<h2>Endorsed Candidates</h2>
+<div id="endorsed-races">
+	<ul class="races">
+		<li>
+			<h3>President and Vice President</h3>
+			<ul class="candidates">
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/jbiden.jpg" alt="Joe Biden" />
+						</div>
+						<div class="name">President Joe Biden</div>
+					</div>
+					<div class="race">President</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/kharris.jpg" alt="Kamala Harris" />
+						</div>
+						<div class="name">Vice President Kamala Harris</div>
+					</div>
+					<div class="race">Vice President</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>Statewide Offices</h3>
+			<ul class="candidates">
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/bcasey.jpg" alt="Bob Casey" />
+						</div>
+						<div class="name">Sen. Bob Casey</div>
+					</div>
+					<div class="race">U.S. Senate</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/rbizzarro.jpg" alt="Ryan Bizzarro" />
+						</div>
+						<div class="name">Rep. Ryan Bizzarro</div>
+					</div>
+					<div class="race">PA Treasurer</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/mkenyatta.jpg" alt="Malcolm Kenyatta" />
+						</div>
+						<div class="name">Rep. Malcolm Kenyatta</div>
+					</div>
+					<div class="race">PA Auditor General</div>
+				</li>
+				<li />
+			</ul>
+		</li>
+		<li>
+			<h3>Congressional Representative</h3>
+			<ul class="candidates">
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/mdean.jpg" alt="Madeline Dean" />
+						</div>
+						<div class="name">Rep. Madeline Dean</div>
+					</div>
+					<div class="race">US Congress, 4th Congressional District</div>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<h3>State Legislature</h3>
+			<ul class="candidates">
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/nnelson.jpg" alt="Napoleon Nelson" />
+						</div>
+						<div class="name">Rep. Napoloen Nelson</div>
+					</div>
+					<div class="race">PA House of Representatives, 154th District</div>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
+
+<h2>Open Races</h2>
+<div id="open-races">
+	<ul class="races">
+		<li>
+			<h3>PA Attorney General</h3>
+			<div class="description">
+				Please vote for any <strong><em>one</em></strong> of these highly qualified candidates:
+			</div>
+			<ul class="candidates">
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/kbradfordgrey.jpg" alt="Keir Bradford-Grey" />
+						</div>
+						<div class="name">Keir Bradford-Grey</div>
+					</div>
+					<div class="race">PA Attorney General</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/edepasquale.png" alt="Eugene DePasquale" />
+						</div>
+						<div class="name">Eugene DePasquale</div>
+					</div>
+					<div class="race">PA Attorney General</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/jkahn.jpg" alt="Joe Kahn" />
+						</div>
+						<div class="name">Joe Kahn</div>
+					</div>
+					<div class="race">PA Attorney General</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/jsolomon.jpg" alt="Jared Solomon" />
+						</div>
+						<div class="name">Jared Solomon</div>
+					</div>
+					<div class="race">PA Attorney General</div>
+				</li>
+				<li>
+					<div class="candidate">
+						<div class="picture">
+							<img src="/images/candidates/2024/jstollsteimer.jpg" alt="Jack Stollsteimer" />
+						</div>
+						<div class="name">Jack Stollsteimer</div>
+					</div>
+					<div class="race">PA Attorney General</div>
+				</li>
+				<li />
+			</ul>
+		</li>
+	</ul>
+</div>
+
+<style>
+	ul.races {
+		list-style: none;
+		border: 4px solid #eee;
+		padding: 0px;
+		margin: 0px;
+	}
+
+	ul.races > li {
+		margin-top: 20px;
+	}
+	ul.races > li:first-of-type {
+		margin-top: 0px;
+	}
+	ul.races > li:first-of-type h3 {
+		margin-top: 0px;
+	}
+
+	ul.candidates {
+		list-style: none;
+		display: flex;
+		flex-wrap: wrap;
+		padding: 5px;
+		margin: 0px;
+	}
+
+	ul.candidates li {
+		min-width: 300px;
+		padding: 0px 10px;
+		margin-top: 20px;
+		flex: 1 0 calc(50% - 20px);
+	}
+
+	div.candidate {
+		text-align: center;
+		font-weight: bold;
+		padding-bottom: 10px;
+	}
+
+	div.candidate div.name {
+		font-size: 1.2em;
+		white-space: nowrap;
+	}
+
+	div.race {
+		text-align: center;
+		background-color: #eee;
+		font-weight: bold;
+	}
+
+	div.picture {
+		margin: auto;
+		max-width: 300px;
+		max-height: 300px;
+		overflow: hidden;
+	}
+
+	div.picture img {
+		max-width: 100%;
+		overflow: hidden;
+		object-fit: cover;
+	}
+
+	div.description {
+		text-align: center;
+		font-style: italic;
+	}
+</style>
