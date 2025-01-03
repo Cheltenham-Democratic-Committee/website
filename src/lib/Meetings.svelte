86b4fe9a07e28156c9812f4ab1f6b8f94dfd8878
Dan Schultz <slifty@gmail.com>
2025-01-03 11:46:08 -0500

    Update meeting dates for 2025

--- src/lib/Meetings.svelte
+++ src/lib/Meetings.svelte
@@ -3,16 +3,16 @@ Join us at <a href="https://maps.app.goo.gl/wMegt4ySXNhxDP8n7" target="_blank"
 >
 @ 7:30 PM:
 <ul>
-	<li>January 8th, 2024</li>
-	<li>February 12th, 2024</li>
-	<li>March 11th, 2024</li>
-	<li>April 8th, 2024</li>
-	<li>May 13th, 2024</li>
-	<li>June 10th, 2024</li>
-	<li>July 8th, 2024</li>
+	<li>January 13th, 2024</li>
+	<li>February 10th, 2024</li>
+	<li>March 10th, 2024</li>
+	<li>April 14th, 2024</li>
+	<li>May 12th, 2024</li>
+	<li>June 9th, 2024</li>
+	<li>July 14th, 2024</li>
 	<li>No meeting in August</li>
-	<li>September 9th, 2024</li>
-	<li>October 7th, 2024 (1st Monday)</li>
-	<li>November 11th, 2024</li>
-	<li>December 9th, 2024</li>
+	<li>September 8th, 2024</li>
+	<li>October 8th, 2024 (1st Monday)</li>
+	<li>November 10th, 2024</li>
+	<li>December 8th, 2024</li>
 </ul>
